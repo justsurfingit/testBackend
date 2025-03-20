2763290911911470145fee312da3f1ea991c791e
justsurfingit <panwararun116@gmail.com>
2025-03-20 09:19:20 +0530

    fixed backend

--- app.py
+++ app.py
@@ -70,4 +70,6 @@ def delete_property(id):
     return '', 204
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
